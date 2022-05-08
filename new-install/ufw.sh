6cd4fb67f25bcb7c6aa0b95d850ef1d83bf075ba
confor <confor@users.noreply.github.com>
2022-05-08 13:48:53 -0400

    ufw.sh: REALLY WARN about ufw killing ssh

--- new-install/ufw.sh
+++ new-install/ufw.sh
@@ -5,8 +5,14 @@ apt install ufw
 ufw allow 22  # ssh
 ufw allow from 10.0.0.2  # bastion host
 
-echo "Enable with: ufw enable"
 printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+printf "\e[91mHEYYYYYY PAY ATTENTION LOOK AT THIS MESSAGE its very important\e[0m\n"
+echo "Enable the firewall with ufw enable"
 echo "WARNING: ufw can interrupt ssh sessions"
 echo "WARNING: your currently configured ports are:"
-grep -rF Port /etc/ssh/sshd_config /etc/ssh/sshd_config.d/
+grep -rF 'Port' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/
